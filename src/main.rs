7278a3933fd6380cc67e041e2b1366110799c4cb
dratyti22 <belousovi783@gmail.com>
2025-10-13 12:54:36 +0300

    feat: create parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use crate::parser::Parser;
+
 mod lexer;
 mod ast;
+mod parser;
 
 fn main() {
     let x = r#"
         var x = 6;
         ver y = 8;
         print(x*y);
+        y = 10 + x * (1+2);
+        print(y);
     "#;
     let y = lexer::lex(x);
     println!("{:?}", y);
+
+    let mut parser = Parser::new(y);
+    let ast = parser.parse();
+    println!("{:?}", ast);
 }
---
+++ src/parser.rs
@@ -0,0 +1,157 @@
+use crate::ast::{Expr, Stmt};
+use crate::lexer::Token;
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    pos: usize,
+}
+
+impl Parser {
+    pub fn new(token: Vec<Token>) -> Self {
+        Self {
+            tokens: token,
+            pos: 0,
+        }
+    }
+    /// получает текущий токен
+    fn peek(&self) -> Option<&Token> {
+        self.tokens.get(self.pos)
+    }
+    ///сдвигает позицию на 1 и возвращает предыдущий токен
+    fn advance(&mut self) -> Option<Token> {
+        if self.pos < self.tokens.len() {
+            self.pos += 1;
+        }
+        self.tokens.get(self.pos - 1).cloned()
+    }
+
+    /// проверка и потребление токена если совподают
+    fn match_token(&mut self, token: &Token) -> bool {
+        if let Some(current) = self.peek() {
+            if std::mem::discriminant(current) == std::mem::discriminant(token) {
+                self.advance();
+                return true;
+            }
+        }
+        false
+    }
+    /// если токен не совпадает, то паника
+    fn expect(&mut self, token: &Token) {
+        if !self.match_token(token) {
+            panic!("Expected {:?} but got {:?}", token, self.peek())
+        }
+    }
+
+    pub fn parse(&mut self) -> Vec<Stmt> {
+        let mut stmts: Vec<Stmt> = Vec::new();
+        while self.peek().is_some() {
+            stmts.push(self.parse_stmt())
+        }
+        stmts
+    }
+    fn parse_stmt(&mut self) -> Stmt {
+        match self.peek() {
+            Some(Token::Var) | Some(Token::Ver) => self.parse_var_decl(),
+            Some(Token::Ident(name)) if name == "print" => self.parse_print(),
+            Some(Token::Ident(_)) => self.parse_assign(),
+
+            _ => panic!("Ожидал ось имя переменной"),
+        }
+    }
+    /// парсинг переменной
+    fn parse_var_decl(&mut self) -> Stmt {
+        let mutable = self.advance(); // тут получаем тип переменной
+        let x = mutable.unwrap();
+        match x {
+            Token::Var => {
+                let name = match self.advance() {
+                    Some(Token::Ident(name)) => name, // тут получаем имя переменной
+                    _ => panic!("Ожидал имя переменной"),
+                };
+                self.expect((&Token::Assign)); // тут проверяем что после имени идет =
+                let value = self.parse_expr(); // тут парсим выражение
+                self.expect((&Token::Semicolon)); // тут проверяем что после выражения идет ;
+                Stmt::Assign { name, value }
+            }
+            Token::Ver => {
+                let name = match self.advance() {
+                    Some(Token::Ident(name)) => name, // тут получаем имя переменной
+                    _ => panic!("Ожидал имя переменной"),
+                };
+                self.expect((&Token::Assign)); // тут проверяем что после имени идет =
+                let value = self.parse_expr(); // тут парсим выражение
+                self.expect((&Token::Semicolon)); // тут проверяем что после выражения идет ;
+                Stmt::VerDecl {
+                    name,
+                    value,
+                    mutable: true,
+                }
+            }
+            _ => panic!("Ожидал переменную"),
+        }
+    }
+
+    fn parse_print(&mut self) -> Stmt {
+        self.advance(); /// пропускаем print
+        self.expect(&Token::LParen);
+        let expr = self.parse_expr();
+        self.expect(&Token::RParen);
+        self.expect(&Token::Semicolon);
+        Stmt::Print(expr)
+    }
+
+    fn parse_expr(&mut self) -> Expr {
+        let mut left = self.parse_primary();
+
+        while let Some(tok) = self.peek() {
+            match tok {
+                Token::Plus => {
+                    self.advance();
+                    let right = self.parse_primary();
+                    left = Expr::Plus(Box::new(left), Box::new(right));
+                }
+                Token::Minus => {
+                    self.advance();
+                    let right = self.parse_primary();
+                    left = Expr::Minus(Box::new(left), Box::new(right));
+                }
+                Token::Star => {
+                    self.advance();
+                    let right = self.parse_primary();
+                    left = Expr::Star(Box::new(left), Box::new(right));
+                }
+                Token::Slash => {
+                    self.advance();
+                    let right = self.parse_primary();
+                    left = Expr::Slash(Box::new(left), Box::new(right));
+                }
+                _ => break,
+            }
+        }
+        left
+    }
+
+    fn parse_primary(&mut self) -> Expr {
+        match self.advance() {
+            Some(Token::Number(n)) => Expr::Num(n),
+            Some(Token::Ident(name)) => Expr::Ident(name),
+            Some(Token::LParen) => {
+                let expr = self.parse_expr();
+                self.expect(&Token::RParen);
+                expr
+            }
+            other => panic!("Ожидал число или переменную: {:?}", other),
+        }
+    }
+
+    fn parse_assign(&mut self) -> Stmt {
+        let name = match self.advance() {
+            Some(Token::Ident(name)) => name,
+            _ => panic!("Ожидал имя переменной"),
+        };
+        self.expect(&Token::Assign);
+        let value = self.parse_expr();
+        self.expect(&Token::Semicolon);
+        Stmt::Assign { name, value }
+    }
+}
